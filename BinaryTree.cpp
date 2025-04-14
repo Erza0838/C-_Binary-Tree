eba3abf44b70f365a9ff79e2045be34ad2aeed64
muhammaderza884@gmail.com <muhammaderza884@gmail.com>
2025-04-15 00:46:21 +0700

    Root node

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 using namespace std;
 
+struct NodeElement
+{
+  int RootNodeValue;
+  int ParentNodeValue;
+} NodeElementStruct;
+
+class LeafNode 
+{
+  public: 
+    int InsertLeafNode() 
+    {
+      cout << "Maskkan leaf node  : ";
+      cin >> NodeElementStruct.RootNodeValue;
+    }
+};
+
 class RootNode
 {
   public: 
-    void InsertRootNode() 
+    int InsertRootNode() 
     {
       cout << "Maskkan root node  : ";
+      cin >> NodeElementStruct.RootNodeValue;
+      switch(NodeElementStruct.RootNodeValue)
+      {
+        case !NULL : 
+        {
+          LeafNode leafNode;
+          leafNode.InsertLeafNode();
+        }
+      }
     }
 };
 
